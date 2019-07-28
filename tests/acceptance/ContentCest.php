0b78a5c32738cc99c1b8d27e3121b2b94f61eaa9
Daniel Primo <delineas@gmail.com>
2019-07-28 20:45:24 +0200

    Add Content createAPage

---
+++ tests/acceptance/ContentCest.php
@@ -0,0 +1,40 @@
+<?php
+
+use \Codeception\Step\Argument\PasswordArgument;
+
+class ContentCest
+{
+
+    public function _before(AcceptanceTester $I)
+    {
+        $I->amOnPage('/user/login');
+        $I->see('Iniciar sesión');
+        $I->amGoingTo('Login as admin');
+        $I->fillField('name', 'admin');
+        $I->fillField('pass', new PasswordArgument('admin'));
+        $I->click('#edit-submit');
+        $I->canSee('admin');
+
+    }
+
+    public function createAPage(AcceptanceTester $I)
+    {
+        $I->amOnPage('/node/add/page');
+
+        //$I->seeResponseCodeIs(200);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
+
+        $I->amGoingTo('Fill page content');
+        $I->fillField('title[0][value]', 'Este es mi título');
+        $I->click('#edit-submit');
+        $I->canSee('Este es mi título', 'h1');
+        
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+        $I->amGoingTo('Logout');
+        $I->click('//a[@href="/user/logout"]');
+    }
+
+}
